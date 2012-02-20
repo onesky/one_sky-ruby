99d2127558fc9a5db43c5c45d05173ff906fe7aa
Matthew Rudy Jacobs <MatthewRudyJacobs@gmail.com>
2012-02-21 02:23:45 +0800

    when adding a :tag, it should be added as a hash argument

--- spec/translation_spec.rb
+++ spec/translation_spec.rb
@@ -5,27 +5,27 @@ describe OneSky::Translation do
   let(:secret)  { "secret" }
   let(:project_name) { "some-project" }
   let(:platform_id) { 123 }
-  
+
   let(:client) { OneSky::Client.new(api_key, secret) }
   let(:project) { client.project(project_name) }
   let(:platform) { project.platform(platform_id) }
   let(:translation) { platform.translation }
-  
+
   describe "input_strings" do
-    
+
     let(:string_array) {
       ["test1", "test2"]
     }
-    
+
     let(:hash_array) {
       [{:string => "test1"}, {:string => "test2"}]
     }
-    
-    
+
+
     context "with an array of strings" do
       it "calls /string/input" do
-        client.should_receive(:post).with("string/input", :input => JSON.dump(hash_array), :tag => "Default", :"platform-id" => platform_id)
-        
+        client.should_receive(:post).with("string/input", :input => JSON.dump(hash_array), :"platform-id" => platform_id)
+
         translation.input_strings(string_array)
       end
     end
@@ -33,51 +33,63 @@ describe OneSky::Translation do
     context "with an array of strings and tag" do
       it "calls /string/input" do
         client.should_receive(:post).with("string/input", :input => JSON.dump(hash_array), :tag => "tagged", :"platform-id" => platform_id)
-        
-        translation.input_strings(string_array, "tagged")
+
+        translation.input_strings(string_array, :tag => "tagged")
       end
     end
 
     context "with an array of hashes" do
       it "calls /string/input" do
-        client.should_receive(:post).with("string/input", :input => JSON.dump(hash_array), :tag => "Default", :"platform-id" => platform_id)
-        
+        client.should_receive(:post).with("string/input", :input => JSON.dump(hash_array), :"platform-id" => platform_id)
+
         translation.input_strings(hash_array)
       end
     end
-    
+
     context "with a complex array of hashes" do
       it "dasherizes the keys" do
         original = [{:string => "Test 1", :string_key => "test1"}]
         dasherized = [{:string => "Test 1", :"string-key" => "test1"}]
-        
-        client.should_receive(:post).with("string/input", :input => JSON.dump(dasherized), :tag => "Default", :"platform-id" => platform_id)
-        
+
+        client.should_receive(:post).with("string/input", :input => JSON.dump(dasherized), :"platform-id" => platform_id)
+
         translation.input_strings(original)
        end
     end
   end
-  
+
   describe "input_string" do
     it "defers to input_strings" do
-      translation.should_receive(:input_strings).with(["test1"], "Default")
+      translation.should_receive(:input_strings).with(["test1"], {})
       translation.input_string("test1")
     end
+
+    it "passes on a :tag" do
+      translation.should_receive(:input_strings).with(["test1"], {:tag => "tagged"})
+      translation.input_string("test1", :tag => "tagged")
+    end
+
   end
-  
+
   describe "input_phrases" do
     it "defers to input_strings" do
-      translation.should_receive(:input_strings).with([{:string_key => "test1", :string => "Test 1"}], "Default")
+      translation.should_receive(:input_strings).with([{:string_key => "test1", :string => "Test 1"}], {})
       translation.input_phrases("test1" => "Test 1")
     end
+
+    it "passes on a :tag" do
+      translation.should_receive(:input_strings).with([{:string_key => "test1", :string => "Test 1"}], {:tag => "tagged"})
+      translation.input_phrases({"test1" => "Test 1"}, :tag => "tagged")
+    end
+
   end
-  
+
   describe "translate" do
-    
+
     let(:string_key) { "test1" }
     let(:locale) { "en_US" }
     let(:string) { "Test 1" }
-    
+
     it "calls /string/translate" do
       client.should_receive(:post).with(
         "string/translate",
@@ -86,13 +98,13 @@ describe OneSky::Translation do
         :translation => string,
         :"platform-id" => platform_id
       )
-      
+
       translation.translate(string_key, locale, string)
     end
   end
-  
+
   describe "output" do
-    
+
     let(:example) do
       {
           "translation" => {
@@ -117,12 +129,12 @@ describe OneSky::Translation do
           "md5" => "95e6198f16ea363291dfa9bc18282545"
       }
     end
-    
+
     it "calls /string/output" do
       client.should_receive(:get).with("string/output", :"platform-id" => platform_id).and_return(example)
       translation.output.should == example["translation"]
     end
-    
+
     context "for_locale" do
       it "calls /string/output with the locale" do
         client.should_receive(:get).with("string/output", :"platform-id" => platform_id, :locale => "en_US").and_return(example)
